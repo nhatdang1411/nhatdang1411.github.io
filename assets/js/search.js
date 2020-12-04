5a7321d78855a7925fcb607f8c75c6735f3b7079
nhatdang1411 <74757824+nhatdang1411@users.noreply.github.com>
2020-12-04 15:47:32 +0700

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -25,7 +25,7 @@
       var pair = vars[i].split('=');
 
       if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+        return decodeURIComponent(pair[1]);
       }
     }
   }
@@ -59,22 +59,3 @@
     }
   }
 })();
-...
-function displaySearchResults(results, store) {
-  var searchResults = document.getElementById('search-results');
-
-  if (results.length) { // Are there any results?
-    var appendString = '';
-
-    for (var i = 0; i < results.length; i++) {  // Iterate over the results
-      var item = store[results[i].ref];
-      appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-      appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
-    }
-
-    searchResults.innerHTML = appendString;
-  } else {
-    searchResults.innerHTML = '<li>No results found</li>';
-  }
-}
-...
